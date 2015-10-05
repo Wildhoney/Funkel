7e433126447d55e2a11d0ca39658bc15cc68a6a7
Wildhoney <adam.timberlake@gmail.com>
2015-10-05 14:53:22 +0100

    Added functions for alpha release

---
+++ src/funkel.js
@@ -0,0 +1,35 @@
+/**
+ * @method identity
+ * @param {*} x
+ * @return {void}
+ */
+export function identity(x) {
+
+}
+
+/**
+ * @method curry
+ * @param {Function} f
+ * @return {Function}
+ */
+export function curry(f) {
+
+}
+
+/**
+ * @method partial
+ * @param {Function} f
+ * @return {Function}
+ */
+export function partial(f) {
+
+}
+
+/**
+ * @method compose
+ * @param {Function[]} fs
+ * @return {Function}
+ */
+export function compose(...fs) {
+
+}
